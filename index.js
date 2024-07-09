721fa496f28b9b8ebf853ef8714e99d8427b3031
Daniel Beaulne <38220914+DBeaulne@users.noreply.github.com>
2024-07-09 03:06:14 -0400

    debug /videos api endpoint

--- index.js
+++ index.js
@@ -3,31 +3,14 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || process.argv[2] || 8080;
 const BACKEND_URL = process.env.BACKEND_URL || "http://localhost";
-const routes = require("./routes/videos");
-const videos = require("./data/videos.json");
+const videoRoutes = require("./routes/videos");
+// const video = require("./data/videos.json");
 
 app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 
-// app.use("/", routes);
-app.get("/videos", (req, res) => {
-	let videoList = videos.map((list) => {
-		return { id: list.id, title: list.title, channel: list.channel, image: list.image };
-	});
-	res.json(videoList);
-	// put api code in here
-	// response data needs to be video list data containing only the id, title
-	// channel and image (path to image)
-});
-
-app.get("/videos/:id", (req, res) => {
-	// add response data here
-	// response data needs to be video details of the video matching the :id
-});
-
-app.post("/vidoes", (req, res) => {
-	// accept the uploaded video and add it to the video list with a created id
-});
+app.use("/assets", express.static("public"));
+app.use("/videos", videoRoutes);
 
 // app listening on port set in .env
 app.listen(PORT, () => {
--- routes/videos.js
+++ routes/videos.js
@@ -3,21 +3,31 @@ const router = express.Router();
 const { v4: uuidv4 } = require("uuid");
 const videos = require("../data/videos.json");
 
-router.get("/videos", (req, res) => {
+router.get("/", (req, res) => {
+	// fetch the entire video list
+	// .map() method creates a new array of objects with only the four required key: value pairs
 	let videoList = videos.map((list) => {
 		return { id: list.id, title: list.title, channel: list.channel, image: list.image };
 	});
 	res.json(videoList);
-	// put api code in here
-	// response data needs to be video list data containing only the id, title
-	// channel and image (path to image)
 });
 
-router.get("/videos/:id", (req, res) => {
+router.get("/:id", (req, res, next) => {
+	const videoId = req.params.id;
+	const isVideoId = (video) => {
+		return video.id === videoId;
+	};
+
+	let video = videos.find(({ id }) => id === videoId);
+	res.json(video);
+
+	// const videoId = req.body.id
 	// add response data here
 	// response data needs to be video details of the video matching the :id
 });
 
-router.post("/vidoes", (req, res) => {
+router.post("/", (req, res) => {
 	// accept the uploaded video and add it to the video list with a created id
 });
+
+module.exports = router;
